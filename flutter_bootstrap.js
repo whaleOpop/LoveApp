83d137d3bb65298b087bd997caae4defa3554ac8
whaleOpop <whaleOpop@users.noreply.github.com>
2024-09-13 21:40:33 +0000

    deploy: 01b30793df4e11f88df310b7d8fa6fa379ff8e94

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1516920169"
+    serviceWorkerVersion: "603145013"
   }
 });
