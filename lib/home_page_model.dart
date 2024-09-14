01943d36e24049be032307bc2ad1077ea075d87a
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-09-15 01:40:12 +0300

    Add home_page
    Add widget
    Add model

---
+++ lib/home_page_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'home_page_widget.dart' show HomePageWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class HomePageModel extends FlutterFlowModel<HomePageWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
