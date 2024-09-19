4f0e45a78cfc538c06ad0b9c22d558a642a51400
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-09-19 13:11:59 +0300

    Create gallery.py
    Create routers for gallery

---
+++ app/routers/gallery.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import crud, schemas
+from app.database import get_db
+
+router = APIRouter()
+
+@router.post("/", response_model=schemas.GalleryResponse)
+def create_gallery(gallery: schemas.GalleryCreate, db: Session = Depends(get_db)):
+    return crud.create_gallery(db, gallery)
+
+@router.get("/{gallery_id}", response_model=schemas.GalleryResponse)
+def read_gallery(gallery_id: int, db: Session = Depends(get_db)):
+    db_gallery = crud.get_gallery(db, gallery_id)
+    if db_gallery is None:
+        raise HTTPException(status_code=404, detail="Gallery not found")
+    return db_gallery
+
