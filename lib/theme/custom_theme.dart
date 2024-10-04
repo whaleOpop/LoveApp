27793b12701794eded7ded35ba57943e8dbb314a
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-04 21:23:34 +0300

    Create custom_theme.dart

---
+++ lib/theme/custom_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: Colors.white,
+    scaffoldBackgroundColor: Colors.black, 
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+        fontFamily: 'Miratrix',
+        color: Colors.white,
+        fontSize: 36,
+        letterSpacing: 0.0,
+        fontWeight: FontWeight.normal,
+      ),
+      titleSmall: TextStyle(
+        fontFamily: 'Miratrix',
+        color: Colors.black,
+        letterSpacing: 0.0,
+        fontWeight: FontWeight.normal,
+      ),
+      bodySmall: TextStyle(
+        fontFamily: 'Montserrat',
+        color: Color(0xFFC0B6C8),
+        fontSize: 12,
+        letterSpacing: 0.0,
+        fontWeight: FontWeight.normal,
+      ),
+    ),
+  );
+
+  static CustomTheme of(BuildContext context) {
+    return CustomTheme();
+  }
+}
