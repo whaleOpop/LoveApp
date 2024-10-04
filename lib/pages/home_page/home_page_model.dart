1be5bc9e9139bf091a4d7ae1909db6317e3ccdf3
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-04 21:22:06 +0300

    Move files to dir

--- lib/pages/home_page/home_page_model.dart
+++ lib/pages/home_page/home_page_model.dart
@@ -1,8 +1,6 @@
 import 'package:flutterflow_ui/flutterflow_ui.dart';
 import 'home_page_widget.dart' show HomePageWidget;
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 class HomePageModel extends FlutterFlowModel<HomePageWidget> {
   @override
