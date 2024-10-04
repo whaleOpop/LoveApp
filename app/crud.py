c0b1e56f32c260ad371be6934a3a507c8c7538cb
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-04 03:10:41 +0300

    Update crud.py
    Add fastapi import HTTPException
    change detail to "details"

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models import User, Gallery, Photo, Couple
-
+from fastapi import HTTPException
 from app.schemas import UserCreate, GalleryCreate, PhotoCreate, CoupleCreate
 
 
@@ -17,14 +17,14 @@ def get_user(db: Session, user_id: int):
 
 
 def delete_user(db: Session, user_id: int):
-    db_user = db.query(User).filter(User.id == user.id).first()
+    db_user = db.query(User).filter(User.id == user_id).first()
 
-    if db.user is None:
+    if db_user is None:
         raise HTTPException(status=404, detail="User is not found")
 
     db.delete(db_user)
     db.commit()
-    return {detail: "User succsesfully deleted"}
+    return {"details": "User succsesfully deleted"}
 
 
 def create_photo(db: Session, photo: PhotoCreate):
@@ -43,11 +43,11 @@ def delete_photo(db: Session, photo_id: int):
     db_photo = db.query(Photo).filter(Photo.id == photo_id).first()
 
     if db.photo is None:
-        raise HTTPExecption(status=404, details="Photo is not found")
+        raise HTTPException(status=404, details="Photo is not found")
 
     db.delete(db_photo)
     db.commit()
-    return {details: "Photo succsesfully deleted"}
+    return {"details": "Photo succsesfully deleted"}
 
 
 def create_gallery(db: Session, gallery: GalleryCreate):
@@ -66,11 +66,11 @@ def delete_gallery(db: Session, gallery_id: int):
     db_gallery = db.query(Gallery).filter(Gallery.id == gallery_id).first()
 
     if db.photo is None:
-        raise HTTpExepcion(status=404, details="Gallery is not found")
+        raise HTTPException(status=404, details="Gallery is not found")
 
     db.delte(db_gallery)
     db.commit()
-    return {details: "Gallery succsesfully deleted"}
+    return {"details": "Gallery succsesfully deleted"}
 
 
 def create_couple(db: Session, couple: CoupleCreate):
@@ -89,8 +89,8 @@ def delete_couple(db: Session, couple_id: int):
     db_couple = db.query(Couple).filter(Couple.id == couple_id).first()
 
     if db.couple is None:
-        raise HTTpExepcion(status=404, details="Couple is not found")
+        raise HTTPException(status=404, details="Couple is not found")
 
     db.delte(db_couple)
     db.commit()
-    return {details: "Couple succsesfully deleted"}
+    return {"details": "Couple succsesfully deleted"}
