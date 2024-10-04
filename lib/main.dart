fd5b04a41c2499d1d6303c4d59f70f86423d173d
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-05 02:59:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:loveapp/pages/home_page/home_page_widget.dart';
 import 'theme/custom_theme.dart';
-
-
+import 'package:flutter/services.dart';
 
 void main() {
-
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
 
