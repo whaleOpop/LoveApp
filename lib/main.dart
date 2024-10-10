888a3f362eda209acc86b590e39458dd95612b80
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-10 19:35:49 +0300

    Update main.dart
    Off debug mod

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Love App',
-        debugShowCheckedModeBanner: true,
+        debugShowCheckedModeBanner: false,
         theme: LoveAppTheme.miratrixLightTheme,
         home: const HomePageWidget());
   }
