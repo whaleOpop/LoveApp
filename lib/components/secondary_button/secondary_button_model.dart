3142dc55b2e2aa9aa454ea8068369af066c7b1c5
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-10 19:22:46 +0300

    Add secondary_button component

---
+++ lib/components/secondary_button/secondary_button_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'secondary_button_widget.dart' show SecondaryButtonWidget;
+import 'package:flutter/material.dart';
+
+class SecondaryButtonModel extends FlutterFlowModel<SecondaryButtonWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
