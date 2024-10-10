0c90a9f3afce82be3f21f46d0d16c871ef752376
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-10 19:22:35 +0300

    Add primary_button component

---
+++ lib/components/primary_button/primary_button_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'primary_button_widget.dart' show PrimaryButtonWidget;
+import 'package:flutter/material.dart';
+
+class PrimaryButtonModel extends FlutterFlowModel<PrimaryButtonWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
