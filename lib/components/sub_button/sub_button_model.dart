c5b1535a951d0bb4b0754e9f9a2a85b6ef264dd2
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-10 19:22:53 +0300

    Add sub_button component

---
+++ lib/components/sub_button/sub_button_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'sub_button_widget.dart' show SubButtonWidget;
+import 'package:flutter/material.dart';
+
+class SubButtonModel extends FlutterFlowModel<SubButtonWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
---
+++ lib/components/sub_button/sub_button_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutterflow_ui/flutterflow_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:loveapp/theme/love_app_theme.dart';
+
+import 'sub_button_model.dart';
+export 'sub_button_model.dart';
+
+class SubButtonWidget extends StatefulWidget {
+  const SubButtonWidget({
+    super.key,
+    String? actionText,
+  }) : this.actionText = actionText ?? 'Action';
+
+  final String actionText;
+
+  @override
+  State<SubButtonWidget> createState() => _SubButtonWidgetState();
+}
+
+class _SubButtonWidgetState extends State<SubButtonWidget> {
+  late SubButtonModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => SubButtonModel());
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: AlignmentDirectional(0, 0),
+      child: FFButtonWidget(
+        onPressed: () {
+          print('Button pressed ...');
+        },
+        text: widget!.actionText,
+        options: FFButtonOptions(
+          height: 40,
+          padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+          iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+          color: Colors.transparent,
+          textStyle: LoveAppTheme.montserratLightTheme.textTheme.titleSmall,
+          elevation: 0,
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+}
