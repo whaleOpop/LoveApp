aed421a5326f4e9f82eb997c89dcaa42c704c133
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-15 23:04:23 +0300

    Update database.py
    update DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./database/database.db"
+DATABASE_URL = "mysql+pymysql://root:0fM94p7u@loveapp-mysql:3306/loveappdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
