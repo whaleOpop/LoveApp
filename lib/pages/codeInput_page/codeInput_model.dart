99052cc67a054992c21044e896a22ec0f52f066f
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-15 21:41:10 +0300

    Update codeInput_model.dart
    clean-up

--- lib/pages/codeInput_page/codeInput_model.dart
+++ lib/pages/codeInput_page/codeInput_model.dart
@@ -3,7 +3,6 @@ import '/components/sub_button/sub_button_widget.dart';
 import 'package:flutterflow_ui/flutterflow_ui.dart';
 
 import 'codeInput_widget.dart' show CodeInputWidget;
-import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:flutter/material.dart';
 
 class CodeInputModel extends FlutterFlowModel<CodeInputWidget> {
