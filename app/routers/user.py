85d8bd70384e0e6be0b771c6d2667fb3b87fa13d
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-17 21:41:10 +0300

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,28 +8,30 @@ router = APIRouter()
 
 @router.post("/register", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db, user)
+    db_user = crud.create_user(db, user)
+    return {"detail": "Login successful", "session_token": db_user.sessionid}
 
 
 @router.post("/login")
 def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_phone(db, phone=user.phone)
 
-    if db_user is None or user.password != db_user.password:
+    if db_user is None or not crud.verify_password(user.password, db_user.password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    return {"detail": "Login successful"}
+    token = crud.update_user_token(db, sessionid=db_user.sessionid)
+    return {"detail": "Login successful", "session_token": token}
 
 
-@router.get("/{user_id}", response_model=schemas.UserResponse)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id)
+@router.get("/{session_id}")
+def read_user(session_id: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_session_id(db, session_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
 @router.delete("/{user_id}")
-def delete_user_endpoint(user_id: int, db: Session = Depends(get_db)):
+def delete_user_endpoint(user_id: str, db: Session = Depends(get_db)):
     crud.delete_user(db=db, user_id=user_id)
     return {"detail": "User successfully deleted"}
