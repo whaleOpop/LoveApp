6ad33ef65972f8ff1d7123022a806ae83e948fb8
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-17 22:30:43 +0300

    Update user.py
    Add check phone

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,6 +8,12 @@ router = APIRouter()
 
 @router.post("/register", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserRegister, db: Session = Depends(get_db)):
+    existing_user = db.query(user).filter(user.phone == user.phone).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400, detail="Номер уже зарегистрирован")
+    
     db_user = crud.create_user(db, user)
     return {"detail": "Login successful", "session_token": db_user.sessionid}
 
@@ -26,10 +32,11 @@ def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
 @router.get("/{session_id}")
 def read_user(session_id: str, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_session_id(db, session_id)
-    
+
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    db_user = crud.update_user_token(db,session_id)
+    
+    db_user = crud.update_user_token(db, session_id)
     return db_user
 
 
