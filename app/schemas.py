6e7b55dd63c0b88e7ae06bc85c5881bee0722305
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-19 11:02:10 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -2,14 +2,17 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
+
 class UserLogin(BaseModel):
     phone: str
     password: str
+
 
 class UserRegister(BaseModel):
     name: str
     phone: str
     password: str
+
 
 class UserCreate(BaseModel):
     name: str
@@ -20,39 +23,48 @@ class UserCreate(BaseModel):
     city: str
     uuid: str
 
+
 class UserResponse(BaseModel):
     id: str
     name: str
     phone: str
-    age: int
-    zodiac: str
-    city: str
-    uuid: str
+    age: Optional[int]
+    zodiac: Optional[str]
+    city: Optional[str]
+    uuid: Optional[str]
+    sessionid: str
     class Config:
         orm_mode = True
+
 
 class GalleryCreate(BaseModel):
     name: str
     description: str
 
+
 class GalleryResponse(BaseModel):
     id: str
     name: str
     description: str
+
     class Config:
         orm_mode = True
+
 
 class PhotoCreate(BaseModel):
     description: str
     photo: bytes
     id_gallery: str
 
+
 class PhotoResponse(BaseModel):
     id: str
     description: str
     id_gallery: str
+
     class Config:
         orm_mode = True
+
 
 class CoupleCreate(BaseModel):
     start_date: datetime
@@ -61,6 +73,7 @@ class CoupleCreate(BaseModel):
     id_para_women: str
     id_gallery: Optional[str]
 
+
 class CoupleResponse(BaseModel):
     id: str
     start_date: datetime
@@ -68,5 +81,6 @@ class CoupleResponse(BaseModel):
     id_para_man: str
     id_para_women: str
     id_gallery: Optional[str]
+
     class Config:
         orm_mode = True
