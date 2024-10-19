95a97f7e554dc21ff74af244524d58eef9a267be
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-19 10:38:14 +0300

    Update database.py
    Add secrets

--- app/database.py
+++ app/database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:0fM94p7u#hZ$@202.181.188.132:3306/loveappdb"
+DATABASE_URL = "mysql+pymysql://root:${{secrets.SSH_PASSWORD}}@${{ secrets.SSH_HOST }}:3306/loveappdb"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
