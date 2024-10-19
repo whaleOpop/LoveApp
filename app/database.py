a463bcdbdc5f7bbbd5e92a8783e23353ee057283
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-19 10:49:01 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+pymysql://root:${{secrets.SSH_PASSWORD}}@${{ secrets.SSH_HOST }}:3306/loveappdb"
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL,
+                       pool_size=20,
+                       max_overflow=10,
+                       pool_recycle=1800,
+                       pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
