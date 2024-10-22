b21e60511fe7797102843d8cbc365bb2d93766af
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-23 02:06:25 +0300

    Update user.py
    Add check uniqe number

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas import UserCreate, UserAuth, UserOut
-from app.crud import create_user, authenticate_user, get_user_by_session, delete_session
+from app.crud import create_user, authenticate_user, get_user_by_session, delete_session,get_user_by_phone
 from app.database import get_db
 
 router = APIRouter()
 
+
 @router.post("/register")
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = get_user_by_phone(db, user.phone)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Номер уже зарегистрирован")
     db_user = create_user(db, user.dict())
+    
     return {"sessionid": db_user.sessionid}
+
 
 @router.post("/login")
 def login(user_auth: UserAuth, db: Session = Depends(get_db)):
