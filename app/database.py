193c95b682b589f8776aac45cdd5e05a74d11922
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-23 00:02:15 +0300

    Update database.py
    Change  pool_recycle=500

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ DATABASE_URL = "mysql+pymysql://root:${{secrets.SSH_PASSWORD}}@${{ secrets.SSH_H
 engine = create_engine(DATABASE_URL,
                        pool_size=20,
                        max_overflow=10,
-                       pool_recycle=1800,
+                       pool_recycle=500,
                        pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
