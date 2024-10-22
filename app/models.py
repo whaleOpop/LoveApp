2ccd9fa4eb9778d52f26a98108982e973c2f4b01
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-23 00:45:42 +0300

    Update models.py
    Re base

--- app/models.py
+++ app/models.py
@@ -1,44 +1,38 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, BLOB
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, LargeBinary, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
-
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(String, primary_key=True, index=True)
+    id = Column(String(255), primary_key=True, index=True)
     name = Column(String, nullable=False)
-    phone = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    photo = Column(BLOB, nullable=True)
+    phone = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    photo = Column(LargeBinary, nullable=True)
     age = Column(Integer, nullable=True)
-    zodiac = Column(String, nullable=True)
-    city = Column(String, nullable=True)
-    uuid = Column(String, unique=True, nullable=True)
-    sessionid = Column(String, unique=True, nullable=False)
-
+    zodiac = Column(String(255), nullable=True)
+    city = Column(String(255), nullable=True)
+    uuid = Column(String(255), nullable=True)
+    sessionid = Column(String(45), unique=True, nullable=False)
 
 class Gallery(Base):
     __tablename__ = "gallery"
-    id = Column(String, primary_key=True, index=True)
+    id = Column(String(255), primary_key=True, index=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
 
+class Couple(Base):
+    __tablename__ = "couple"
+    id = Column(String(255), primary_key=True, index=True)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=True)
+    id_para_man = Column(String(255), ForeignKey('users.id'), nullable=False)
+    id_para_women = Column(String(255), ForeignKey('users.id'), nullable=False)
+    id_gallery = Column(String(255), ForeignKey('gallery.id'), nullable=True)
 
 class Photo(Base):
     __tablename__ = "photo"
-    id = Column(String, primary_key=True, index=True)
+    id = Column(String(255), primary_key=True, index=True)
     description = Column(String, nullable=False)
-    photo = Column(BLOB, nullable=False)
-    id_gallery = Column(String, ForeignKey('gallery.id'))
-
-
-class Couple(Base):
-    __tablename__ = "couple"
-    id = Column(String, primary_key=True, index=True)
-    start_date = Column(DateTime, nullable=False)
-    end_date = Column(DateTime, nullable=True)
-    id_para_man = Column(String, ForeignKey('users.id'))
-    id_para_women = Column(String, ForeignKey('users.id'))
-    id_gallery = Column(String, ForeignKey('gallery.id'), nullable=True)
+    photo = Column(LargeBinary, nullable=False)
+    id_gallery = Column(String(255), ForeignKey('gallery.id'), nullable=True)
