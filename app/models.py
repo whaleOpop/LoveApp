5b72dce071ad85dc75ce0285c8d115106a5e83ef
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-10-23 01:11:00 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Integer, LargeBinary, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from database import Base
+from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
